94ef3effa87c7f809e593946fadc6bddd98cae52
Mino <eng.m.sida@gmail.com>
2018-03-13 07:47:02 +0200

    upload images in products form

--- routes/products.js
+++ routes/products.js
@@ -2,13 +2,21 @@ var express = require ('express');
 var router = express.Router();
 var bodyParser = require ('body-parser');
 var bodyParserMid = bodyParser.urlencoded();
+var fs = require('fs');
+
+var multer = require('multer');
+var uploadMid = multer({
+  dest:"./public/images"
+});
 
 router.get('/add',function(req,resp){
   resp.render('products/add');
 });
 
-router.post('/add',bodyParserMid,function(req,resp){
-  resp.render('products/add');
+router.post('/add',uploadMid.single('proimg'),function(req,resp){
+  //resp.render('products/add');
+  fs.renameSync(req.file.path,req.file.destination+"/"+req.file.originalname);
+  resp.json(req.file);
 
 });
 
